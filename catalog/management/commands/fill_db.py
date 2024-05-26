f963432c8bb782d48bc5e31b43f34160a587aa9c
alexcy <bondaralexcy@gmail.com>
2024-05-26 14:56:55 +0300

    Remove mistakes

--- catalog/management/commands/fill_db.py
+++ catalog/management/commands/fill_db.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
 
     @staticmethod
     def json_read():
-        with open('catalog.json', 'r', encoding='utf8') as file:
+        with open("catalog.json", "r", encoding="utf8") as file:
             data = json.load(file)
             return data
 
@@ -19,23 +19,28 @@ class Command(BaseCommand):
         Category.objects.all().delete()
 
         for i in Command.json_read():
-            if i['model'] == 'catalog.category':
+            if i["model"] == "catalog.category":
                 category_for_create.append(
-                    Category(name=i.get('fields').get('name'), pk=i.get('pk'))
+                    Category(name=i.get("fields").get("name"), pk=i.get("pk"))
                 )
             else:
                 continue
         Category.objects.bulk_create(category_for_create)
 
         for i in Command.json_read():
-            if i['model'] == 'catalog.product':
+            if i["model"] == "catalog.product":
                 product_for_create.append(
-                    Product(pk=i.get('pk'), name=i.get('fields').get('name'),
-                            description=i.get('fields').get('description'), image=i.get('fields').get('image'),
-                            category=Category.objects.get(pk=i['fields']['category']), price=i.get('fields').get('price'),
-                            created_at=i.get('fields').get('created_at'),
-                            updated_at=i.get('fields').get('updated_at')
-                            ))
+                    Product(
+                        pk=i.get("pk"),
+                        name=i.get("fields").get("name"),
+                        description=i.get("fields").get("description"),
+                        image=i.get("fields").get("image"),
+                        category=Category.objects.get(pk=i["fields"]["category"]),
+                        price=i.get("fields").get("price"),
+                        created_at=i.get("fields").get("created_at"),
+                        updated_at=i.get("fields").get("updated_at"),
+                    )
+                )
             else:
                 continue
         Product.objects.bulk_create(product_for_create)
